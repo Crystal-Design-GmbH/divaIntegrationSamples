b2868eea04ca2b2a146f40035776eeec06dbddfd
Mathias Mahlknecht <mathias.mahlknecht@crystal-design.com>
2021-10-13 16:32:14 +0200

    first commit

---
+++ next-js-example/pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+import dynamic from 'next/dynamic'
+
+const DivaWebPlanner = dynamic(
+  () => import('./DivaWebplanner'),
+  { ssr: false }
+)
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Example Diva Webplanner</title>
+        <meta name="description" content="An example next.js integration for the Diva Webplanner" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+          <DivaWebPlanner />
+      </main>
+    </div>
+  )
+}
